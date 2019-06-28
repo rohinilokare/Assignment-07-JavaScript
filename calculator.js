e38d10d4b070431d9250e767a9a2044309176199
rohinilokare <rohini@treeni.com>
2019-06-28 19:44:42 +0530

    modification::Java script::indentation

--- calculator.js
+++ calculator.js
@@ -14,113 +14,112 @@ function Calculator(){
     max_arr = compare_arr[0]+compare_arr[1]+compare_arr[2]
     min_arr = compare_arr[0]+compare_arr[1]+compare_arr[2]
 
+    this.addition = function(a,b){
+      add = `${Number(a) +  Number(b)}`
+      console.log("Answer ",add)
+      console.log('-----------------------------')
+    }
 
-  this.addition = function(a,b){
-    add = `${Number(a) +  Number(b)}`
-    console.log("Answer ",add)
-    console.log('-----------------------------')
-  }
+    this.substraction = function(a,b){
+      minus = `${Number(a) -  Number(b)}`
+      console.log("Answer : ",minus)
+      console.log('-----------------------------')
+    }
 
-  this.substraction = function(a,b){
-    minus = `${Number(a) -  Number(b)}`
-    console.log("Answer : ",minus)
-    console.log('-----------------------------')
+    this.multiplication = function(a,b){
+      mul = `${Number(a) *  Number(b)}`
+      console.log("Answer : ",mul)
+      console.log('-----------------------------')
+    }
 
-  }
+    this.division = function(a,b){
+      div = `${Number(a) /  Number(b)}`
+      console.log("Answer : ",div)
+      console.log('-----------------------------')
+    }
 
-  this.multiplication = function(a,b){
-    mul = `${Number(a) *  Number(b)}`
-    console.log("Answer : ",mul)
-    console.log('-----------------------------')
-  }
+    this.max = function(compare_arr){
+      if(compare_arr[4] > compare_arr[6]){
+        console.log("max number is :"+compare_arr[4])
+      }
+      else{
+        console.log("max number is :"+compare_arr[6])
+      }
+      console.log('-----------------------------')
+    }
 
-  this.division = function(a,b){
-    div = `${Number(a) /  Number(b)}`
-    console.log("Answer : ",div)
-    console.log('-----------------------------')
-  }
-  this.max = function(compare_arr){
-    if(compare_arr[4] > compare_arr[6]){
-      console.log("max number is :"+compare_arr[4])
+    this.min = function(compare_arr){
+    if(compare_arr[4] < compare_arr[6]){
+      console.log("min number is :"+compare_arr[4])
     }
     else{
-      console.log("max number is :"+compare_arr[6])
+      console.log("min number is :"+compare_arr[6])
     }
     console.log('-----------------------------')
+    }
+
+      if(arr[1] == '+'){
+         this.addition(arr[0],arr[2])
+         if(arr[3] == '+'){
+          this.addition(add,arr[4])
+         }else if(arr[3] == '-'){
+          this.substraction(add,arr[4])
+         }else if(arr[3] == '*'){
+          this.multiplication(add,arr[4])
+         } else if(arr[3] == '/'){
+          this.division(add,arr[4])
+         }
+      }
+
+      else if(arr[1] == '-'){
+         this.substraction(arr[0],arr[2])
+         if(arr[3] == '+'){
+          this.addition(minus,arr[4])
+         }else if(arr[3] == '-'){
+          this.substraction(minus,arr[4])
+         }else if(arr[3] == '*'){
+          this.multiplication(minus,arr[4])
+         } else if(arr[3] == '/'){
+          this.division(minus,arr[4])
+         }
+      }
+
+      else if (arr[1] == '*'){
+         this.multiplication(arr[0],arr[2])
+         if(arr[3] == '+'){
+          this.addition(mul,arr[4])
+         }else if(arr[3] == '-'){
+          this.substraction(mul,arr[4])
+         }else if(arr[3] == '*'){
+          this.multiplication(mul,arr[4])
+         } else if(arr[3] == '/'){
+          this.division(mul,arr[4])
+         }
+      }
+
+      else if (arr[1] == '/'){
+        this.division(arr[0],arr[2])
+         if(arr[3] == '+'){
+          this.addition(div,arr[4])
+         }else if(arr[3] == '-'){
+          this.substraction(div,arr[4])
+         }else if(arr[3] == '*'){
+          this.multiplication(div,arr[4])
+         } else if(arr[3] == '/'){
+          this.division(div,arr[4])
+         }
+      }
+
+      else if (max_arr == 'max')
+      {
+        this.max(compare_arr)
+      }
+
+      else if (min_arr == 'min'){
+        this.min(compare_arr)
+      }
+
   }
-
-  this.min = function(compare_arr){
-  if(compare_arr[4] < compare_arr[6]){
-    console.log("min number is :"+compare_arr[4])
-  }
-  else{
-    console.log("min number is :"+compare_arr[6])
-  }
-  console.log('-----------------------------')
-  }
-
-    if(arr[1] == '+'){
-       this.addition(arr[0],arr[2])
-       if(arr[3] == '+'){
-        this.addition(add,arr[4])
-       }else if(arr[3] == '-'){
-        this.substraction(add,arr[4])
-       }else if(arr[3] == '*'){
-        this.multiplication(add,arr[4])
-       } else if(arr[3] == '/'){
-        this.division(add,arr[4])
-       }
-    }
-
-    else if(arr[1] == '-'){
-       this.substraction(arr[0],arr[2])
-       if(arr[3] == '+'){
-        this.addition(minus,arr[4])
-       }else if(arr[3] == '-'){
-        this.substraction(minus,arr[4])
-       }else if(arr[3] == '*'){
-        this.multiplication(minus,arr[4])
-       } else if(arr[3] == '/'){
-        this.division(minus,arr[4])
-       }
-    }
-
-    else if (arr[1] == '*'){
-       this.multiplication(arr[0],arr[2])
-       if(arr[3] == '+'){
-        this.addition(mul,arr[4])
-       }else if(arr[3] == '-'){
-        this.substraction(mul,arr[4])
-       }else if(arr[3] == '*'){
-        this.multiplication(mul,arr[4])
-       } else if(arr[3] == '/'){
-        this.division(mul,arr[4])
-       }
-    }
-
-    else if (arr[1] == '/'){
-      this.division(arr[0],arr[2])
-       if(arr[3] == '+'){
-        this.addition(div,arr[4])
-       }else if(arr[3] == '-'){
-        this.substraction(div,arr[4])
-       }else if(arr[3] == '*'){
-        this.multiplication(div,arr[4])
-       } else if(arr[3] == '/'){
-        this.division(div,arr[4])
-       }
-    }
-
-    else if (max_arr == 'max')
-    {
-      this.max(compare_arr)
-    }
-
-    else if (min_arr == 'min'){
-      this.min(compare_arr)
-    }
-    }
-
 }
 
 calculator = new Calculator();
