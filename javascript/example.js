0de743c18be40aabf62e7a38368072926561b93b
rohinilokare <rohini@treeni.com>
2019-07-08 19:40:23 +0530

    feature::added button dynamically

--- javascript/example.js
+++ javascript/example.js
@@ -1,34 +1,66 @@
-var count = 0
-function validate() {
-  if( document.myForm.lable.value == "" ) {
-     alert( "Please provide your lable!" );
-     document.myForm.lable.focus() ;
-     return false;
-  }
-  if( document.myForm.color.value == "" ) {
-     alert( "Please provide your color!" );
-     document.myForm.color.focus() ;
-     return false;
-  }
-  return( true );
+var count = 0 ;
+var label_array = [];
+// function validate() {
+// 	console.log("in validate")
+//   if( document.myForm.label.value == "" ) {
+//      alert( "Please provide your label!" );
+//      document.myForm.label.focus() ;
+//      return false;
+//   }
+//   if( document.myForm.color.value == "" ) {
+//      alert( "Please provide your color!" );
+//      document.myForm.color.focus() ;
+//      return false;
+//   }
+//   getlabel();
+//   // return( true );
+// }
 
-  getLable();
-  myFunction();
+function showform(){
+document.getElementById("myForm").style.display="block";
 }
 
-  function getLable(){
-	var lable =document.getElementById("lable").value;
-    document.getElementById("countername").innerHTML = "lable";
-  var color =document.getElementById("color").value;
-    document.getElementById("countername").value = color;
+function submitForm(){
+    //document.getElementById("f1").submit();
+    console.log('in get label');
+    var label =document.getElementById("label").value ;
+    var color =document.getElementById("color").value;
+    label_array_fun(label,color);
+    //document.getElementById("countername").innerHTML = label ;
+    //document.getElementById("myForm").style.display="none"
+      display_button();
+    refreshForm();
 }
+
+function label_array_fun(label,color){
+  var counter = {label,color}
+  label_array.push(counter);
+  console.log(label_array)
+
+}
+
+function display_button(){
+var length = label_array.length
+    for(var i=0;i<length;i++){
+     var btn = document.createElement("BUTTON");
+     btn.innerHTML = '    <div class="button_group"><button id=counter_minus onclick="return(decrement());"><span class = "minus"> - </span></button><button id="countername">' +label_array[i].label +'</button><button id="countervalue">0</button><button onclick="return(increment());"> <span class = "plus"> + </span></button></div>'
+      document.body.appendChild(btn);
+  }
+}
+
+function refreshForm(){
+    document.getElementById("label").value = '';
+    document.getElementById("color").value = '';
+}
+
 function increment(){
-	count = count + 1
-	document.getElementById("countervalue").innerHTML = count
+	count = count + 1;
+	document.getElementById("countervalue").innerHTML = count;
+  //document.getElementById("countername").innerHTML = "label"
 }
 
 function decrement(){
-	count = count -1
-	document.getElementById("countervalue").innerHTML = count
-
+	count = count -1;
+	document.getElementById("countervalue").innerHTML = count;
 }
+
